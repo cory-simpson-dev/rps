7f8abafe80f155aad15baa63c11de99a4f6ae226
Cory Simpson <cory.simpson.dev@gmail.com>
2022-09-24 20:15:48 -0700

    added single post & update & delete backend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -12,6 +12,20 @@ const getPosts = asyncHandler(async (req, res) => {
     res.status(200).json(posts)
 })
 
+// @desc    Get user post
+// @route   GET /api/posts/:id
+// @access  Public
+const getPost = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id)
+  
+    if(!post) {
+        res.status(404)
+        throw new Error('Post not found')
+    }
+  
+    res.status(200).json(post)
+})
+
 // @desc    Create new post
 // @route   POST /api/posts
 // @access  Private
@@ -39,7 +53,69 @@ const createPost = asyncHandler(async (req, res) => {
     res.status(201).json(post)
 })
   
+// @desc    Delete post
+// @route   DELETE /api/posts/:id
+// @access  Private
+const deletePost = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id)
+  
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+  
+    const post = await Post.findById(req.params.id)
+  
+    if(!post) {
+        res.status(404)
+        throw new Error('Post not found')
+    }
+  
+    if(post.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not Authorized')
+    }
+  
+    // removes post from db
+    await post.remove()
+  
+    res.status(200).json({success: true})
+})
+  
+// @desc    Update post
+// @route   PUT /api/posts/:id
+// @access  Private
+const updatePost = asyncHandler(async (req, res) => {
+    // Get user using the id in the JWT
+    const user = await User.findById(req.user.id)
+  
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+  
+    const post = await Post.findById(req.params.id)
+  
+    if(!post) {
+        res.status(404)
+        throw new Error('Post not found')
+    }
+  
+    if(post.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not Authorized')
+    }
+  
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  
+    res.status(200).json(updatedPost)
+})
+
 module.exports = {
     getPosts,
-    createPost
+    getPost,
+    createPost,
+    deletePost,
+    updatePost
 }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 // const upload = require("../middleware/multer");
-const {getPosts, createPost} = require('../controllers/postController')
+const {getPosts, getPost, createPost, deletePost, updatePost} = require('../controllers/postController')
 const { protect } = require('../middleware/authMiddleware')
 
 // router.route allows additional methods to be chained on (i.e. get/post)
 router.route('/').get(getPosts).post(protect, createPost)
+router.route('/:id').get(getPost).delete(protect, deletePost).put(protect, updatePost)
 
 module.exports = router;
