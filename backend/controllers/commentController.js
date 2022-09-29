2a72f5a31835225ff09086205cb5ca01b2888716
Cory Simpson <cory.simpson.dev@gmail.com>
2022-09-29 15:14:45 -0700

    added comment voting backend

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -17,7 +17,7 @@ const getComments = asyncHandler(async (req, res) => {
         throw new Error('Post not found')
     }
 
-    const comments = await Comment.find({post: req.params.postId})
+    const comments = await Comment.find({post: req.params.postId}).sort({'createdAt': -1})
     res.status(200).json(comments)
 })
 
@@ -34,9 +34,103 @@ const addComment = asyncHandler(async (req, res) => {
     })
   
     res.status(200).json(comment)
-  })
+})
+
+// @desc    Upvote comment
+// @route   PUT /api/posts/:postId/comments/upvoteComment/:commentId
+// @access  Private
+const handleCommentUpvote = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.postId)
+
+  if(!post) {
+      res.status(404)
+      throw new Error('Post not found')
+  }
+
+  const comment = await Comment.findById(req.params.commentId)
+
+  if(!comment) {
+      res.status(404)
+      throw new Error('Comment not found')
+  }
+
+  let handledComment
+
+  // if comment upvotedBy user already, remove user from comment.upvotedBy
+  if (comment.upvotedBy.indexOf(req.user.id) > -1) {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $pull: {'upvotedBy': req.user.id}
+          }, { new: true }
+      )
+  // if comment previously downvoted by user, remove from down votes && add to upvotes
+  } else if (comment.downvotedBy.indexOf(req.user.id) > -1) {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $pull: {'downvotedBy': req.user.id},
+              $push: {'upvotedBy': req.user.id}
+          }, { new: true }
+      )
+  // otherwise, add user to upvotedBy
+  } else {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $push: {'upvotedBy': req.user.id}
+          }, { new: true })
+  }
+
+  res.status(200).json(handledComment)
+})
+
+// @desc    Downvote comment
+// @route   PUT /api/posts/:postId/comments/downvoteComment/:commentId
+// @access  Private
+const handleCommentDownvote = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.postId)
+
+  if(!post) {
+      res.status(404)
+      throw new Error('Post not found')
+  }
+
+  const comment = await Comment.findById(req.params.commentId)
+
+  if(!comment) {
+      res.status(404)
+      throw new Error('Comment not found')
+  }
+
+  let handledComment
+
+  // if comment downvotedBy user already, remove user from comment.downvotedBy
+  if (comment.downvotedBy.indexOf(req.user.id) > -1) {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $pull: {'downvotedBy': req.user.id}
+          }, { new: true }
+      )
+  // if post previously upvoted by user, remove from up votes && add to downvotes
+  } else if (comment.upvotedBy.indexOf(req.user.id) > -1) {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $pull: {'upvotedBy': req.user.id},
+              $push: {'downvotedBy': req.user.id}
+          }, { new: true }
+      )
+  // otherwise, add user to downvotedBy
+  } else {
+      handledComment = await Comment.findByIdAndUpdate(req.params.commentId, 
+          {
+              $push: {'downvotedBy': req.user.id}
+          }, { new: true })
+  }
+
+  res.status(200).json(handledComment)
+})
 
 module.exports = {
     getComments,
     addComment,
+    handleCommentUpvote,
+    handleCommentDownvote,
 }
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express")
 // mergeParams allows us to use post routes
 const router = express.Router({ mergeParams: true})
-const {getComments, addComment} = require('../controllers/commentController')
+const {getComments, addComment, handleCommentUpvote, handleCommentDownvote } = require('../controllers/commentController')
 const { protect } = require('../middleware/authMiddleware')
 
 // router.route allows additional methods to be chained on (i.e. get/post)
 // route /api/posts/:postId/comments
 router.route('/').get(getComments).post(protect, addComment)
+router.put('/upvoteComment/:commentId', protect, handleCommentUpvote)
+router.put('/downvoteComment/:commentId', protect, handleCommentDownvote)
 
 module.exports = router;
