04670b87834f014a13f60be74ca6c6a09eba0c6a
Cory Simpson <cory.simpson.dev@gmail.com>
2022-10-03 12:10:21 -0700

    added login && register styles

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,13 @@
 import {useState, useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import {toast} from 'react-toastify'
-import {FaUser} from 'react-icons/fa'
 // useSelector allows us to select from global state (i.e. user, isSuccess value, message)
 // useDispatch dispatches actions (i.e. register)
 import {useSelector, useDispatch} from 'react-redux'
 import {register, reset} from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
+// styles
+import { LockClosedIcon } from '@heroicons/react/20/solid'
 
 function Register() {
     // instead of separate states for every field, use formData
@@ -68,70 +69,110 @@ function Register() {
     }
     
     return (
-      <>
-        <section className="heading">
-            <h1>
-                <FaUser /> Register
-            </h1>
-            <p>Please create an account</p>
-        </section>
-
-        <section className="form">
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <input 
-                    type="text" 
-                    className="form-control" 
-                    id='name' 
-                    name='name'
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Welcome to the PLUR Life
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Otherwise,{' '}
+              <Link to='/login'>
+                <span className="font-medium text-indigo-600 hover:text-indigo-500">
+                  welcome back
+                </span>
+              </Link>
+            </p>
+          </div>
+          <form onSubmit={onSubmit} className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+                <div>
+                <label htmlFor="username" className="sr-only">
+                  Username
+                </label>
+                <input
+                    onChange={onChange}
                     value={name} 
-                    onChange={onChange} 
-                    placeholder='Enter your name'
+                    id="username"
+                    name="name"
+                    type="text"
                     required
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="email" 
-                    className="form-control" 
-                    id='email' 
-                    name='email'
+                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Username"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                    onChange={onChange}
                     value={email} 
-                    onChange={onChange} 
-                    placeholder='Enter your email'
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
                     required
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    id='password' 
-                    name='password'
-                    value={password} 
-                    onChange={onChange} 
-                    placeholder='Enter password'
+                    className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                    onChange={onChange}
+                    value={password}
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
                     required
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                    type="password" 
-                    className="form-control" 
-                    id='password2' 
-                    name='password2'
-                    value={password2} 
-                    onChange={onChange} 
-                    placeholder='Confirm password'
+                    className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Password"
+                />
+              </div>
+              <div>
+                <label htmlFor="password2" className="sr-only">
+                  Confirm Password
+                </label>
+                <input
+                    onChange={onChange}
+                    value={password2}
+                    id="password2"
+                    name="password2"
+                    type="password"
+                    autoComplete="current-password"
                     required
-                    />
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-block">Submit</button>
-                </div>
-            </form>
-        </section>
-      </>
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Confirm password"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
     )
   }
   
