1bef7f92a443a4c1d987321b39dd66311edaa4ad
Cory Simpson <cory.simpson.dev@gmail.com>
2022-10-05 10:33:26 -0700

    updated package.json & server.js for heroku hosting

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // commonjs syntax (as opposed to React's ES2015 syntax (import))
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -16,14 +17,24 @@ app.use(express.json())
 // allows to send urlencoded
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to the Support Desk API'})
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/posts', require('./routes/postRoutes'))
 app.use('/api/messaging', require('./routes/messageRoutes'))
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'Welcome to the Real PLUR Life API'})
+    })
+}
 
 app.use(errorHandler)
 
