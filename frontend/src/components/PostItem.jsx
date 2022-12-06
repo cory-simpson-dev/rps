1879c227d544d32fa054bbd5814c2681beece24f
Cory Simpson <cory.simpson.dev@gmail.com>
2022-12-06 15:39:15 -0800

    fixed post responsiveness

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -20,8 +20,8 @@ function PostItem({post}) {
   }
 
   return (
-      <div className="container mx-auto p-3 mb-6 rounded-sm shadow hover:shadow-lg grid grid-cols-[60px_minmax(250px,_1fr)] grid-rows-1 overflow-hidden">
-        <div className="grid grid-cols-1 place-content-center">
+      <div className="container mx-auto p-3 mb-6 rounded-sm shadow hover:shadow-lg flex flex-col md:grid md:grid-cols-[60px_minmax(250px,_1fr)] grid-rows-1 overflow-hidden">
+        <div className="hidden md:grid md:grid-cols-1 place-content-center">
           <VotingButtons 
             item={post}
             upvoteItem={upvotePost}
@@ -29,23 +29,37 @@ function PostItem({post}) {
             dispatchData={{postId, userId}}
           />
         </div>
-      <Link className="overflow-hidden" to={`/post/${post._id}`}>
-        <div className="flex flex-col">
-          <p className='truncate text-sm'>Posted by <Link to={`/user/${post.username}`} className='text-primary hover:text-purple-600'>{post.username}</Link> <TimeSince item={post} /></p>
-          <h4 className='break-normal text-lg font-semibold'>{post.title}</h4>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className='md:hidden'>
+              <VotingButtons 
+                item={post}
+                upvoteItem={upvotePost}
+                downvoteItem={downvotePost}
+                dispatchData={{postId, userId}}
+                />
+            </div>
+            
+            <Link className="flex flex-col justify-center w-full" to={`/post/${post._id}`}>
+              <p className='truncate text-sm'>Posted by <Link to={`/user/${post.username}`} className='text-primary hover:text-purple-600'>{post.username}</Link> <TimeSince item={post} /></p>
+              <h4 className='break-normal text-base md:text-lg font-semibold'>{post.title}</h4>
+            </Link>
+          </div>
+          
           {post.mediaType === 'image' && 
-            <figure className="py-2 mx-auto">
+            <figure className="mx-auto">
               <img className='md:max-w-sm' src={post.mediaUrl} alt={post.mediaUrl}/>
             </figure>
           }
           {post.mediaType === 'video' && 
-            <figure className="py-2 mx-auto">
-              <ReactPlayer url={post.mediaUrl} />
-            </figure>
+            <ReactPlayer 
+              className="w-full object-cover" 
+              url={post.mediaUrl} 
+              width='100%'  
+            />
           }
           <div className='max-h-40 truncate whitespace-pre-wrap text-xs sm:text-base'>{parse(post.body)}</div>
         </div>
-      </Link>
       </div>
   )
 }
--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -91,8 +91,8 @@ function Post() {
   
   return (
     <>
-      <div className="container mx-auto p-3 mb-6 rounded-sm shadow hover:shadow-lg grid grid-cols-[60px_minmax(250px,_1fr)]">
-        <div className="grid grid-cols-1 place-content-center">
+      <div className="container mx-auto p-3 mb-6 rounded-sm shadow hover:shadow-lg flex flex-col md:grid md:grid-cols-[60px_minmax(250px,_1fr)]">
+        <div className="hidden md:grid md:grid-cols-1 place-content-center">
           <VotingButtons 
             item={post}
             upvoteItem={upvotePost}
@@ -100,23 +100,37 @@ function Post() {
             dispatchData={{postId, userId}}
           />
         </div>
-        <div className="flex flex-col">
-          <p className='truncate text-sm'>Posted by <Link to={`/user/${post.username}`} className='text-primary hover:text-purple-600'>{post.username}</Link> <TimeSince item={post} /></p>
-          <h4 className='text-lg font-semibold'>{post.title}</h4>
-          {post.mediaType === 'image' && 
-            <figure className="py-2 mx-auto">
-              <img className="md:max-w-lg" src={post.mediaUrl} alt={post.mediaUrl}/>
-            </figure>
-          }
-          {post.mediaType === 'video' && 
-            <figure className="py-2 mx-auto">
-              <ReactPlayer url={post.mediaUrl} />
-            </figure>
-          }
-          <div className="whitespace-pre-wrap">{parse(postBody)}</div>
-          <p className='text-sm text-gray-500'>{comments.length} comments</p>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <div className='md:hidden'>
+              <VotingButtons 
+                item={post}
+                upvoteItem={upvotePost}
+                downvoteItem={downvotePost}
+                dispatchData={{postId, userId}}
+              />
+            </div>
+            <div className="flex flex-col justify-center w-full">
+              <p className='truncate text-sm'>Posted by <Link to={`/user/${post.username}`} className='text-primary hover:text-purple-600'>{post.username}</Link> <TimeSince item={post} /></p>
+              <h4 className='text-lg font-semibold'>{post.title}</h4>
+            </div>
+          </div>
+            {post.mediaType === 'image' && 
+              <figure className="mx-auto">
+                <img className="md:max-w-lg" src={post.mediaUrl} alt={post.mediaUrl}/>
+              </figure>
+            }
+            {post.mediaType === 'video' && 
+            <ReactPlayer 
+              className="w-full object-cover" 
+              url={post.mediaUrl} 
+              width='100%'  
+            />
+            }
+            <div className="whitespace-pre-wrap">{parse(postBody)}</div>
+            <p className='text-sm text-gray-500'>{comments.length} comments</p>
+          </div>
         </div>
-      </div>
       <header className="ticket-header">
         <h2>Comments</h2>
       </header>
